d7771ea5f0089d882d92e3009654d6351238b90d
Luke Abernethy <luke.abernethy@puppet.com>
2020-03-02 13:03:30 +0000

    Adding Tag Component

--- packages/react-components/source/index.js
+++ packages/react-components/source/index.js
@@ -28,6 +28,7 @@ import Tabs from './react/library/tabs';
 import Breadcrumb from './react/library/breadcrumb';
 import ConfirmationModal from './react/library/confirmation-modal';
 import Code from './react/library/code';
+import Tag from './react/library/tag';
 
 export {
   ActionSelect,
@@ -57,4 +58,5 @@ export {
   Breadcrumb,
   ConfirmationModal,
   Code,
+  Tag,
 };
